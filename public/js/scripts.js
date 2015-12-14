8230776c1adb171d7ba8c4276f8357a8ae757d3d
etsipis <etsipis@terracognita.gr>
2015-12-14 14:11:19 +0200

    5

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -81,3 +81,11 @@
    }
    return "";
  }
+  var amountScrolled = 300;
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > amountScrolled) {
+      $('a.back-to-top').fadeIn('slow');
+    } else {
+      $('a.back-to-top').fadeOut('slow');
+    }
+  });
